435166d527c90bc94225ead59382eae5263e845f
Jinx66 <1577478705@qq.com>
2025-02-06 01:24:46 +0800

    fix

--- source/src/js/main.js
+++
@@ -1,8 +0,0 @@
-// 写这里应该没人发现吧😃😃😃
-console.log("%c cjm天天开心哈哈哈哈哈哈 %c https://jinx66.gitee.io/","color:#fff;font-size: 15px;font-family: serif;background:linear-gradient(90deg,#ff4d91,#ff4d91);padding:15px 10px;","color:#000;background:linear-gradient(90deg,#ff4d91,#ffffff);padding:17px 10px 15px 10px;font-size: 13px");
-
-    //添加iframe标签
-    var body = document.getElementsByTagName("body");
-    var div = document.createElement("div");
-    div.innerHTML = '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=525427197&auto=1&height=66"></iframe>';
-    document.body.appendChild(div);
